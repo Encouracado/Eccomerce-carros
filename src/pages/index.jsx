3d6dc46a5b27ec10ebca17507145321c547e9269
cleison alexandre azevedo pereira <cleison.alexandre.azevedo@gmail.com>
2021-06-08 22:57:17 -0300

    add divs

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div name="wrapper" className="flex flex-col h-full w-full bg-blue-200  dark:bg-gray-800">
+    <div className="container mx-auto px-500 bg-red-500 dark:text-white h-200">Header</div>
+    <div>Carrosel</div>
+    <div>Products</div>
+    </div >
+  )
+}
